ac4dce58f6d4c3955f84ca17dce0ab424d903473
Roman <mashenko@gmail.com>
2017-01-16 14:21:06 +0300

    Initial commit

---
+++ src/main/java/com/solanteq/assingment/HomeAssingmentApplication.java
@@ -0,0 +1,12 @@
+package com.solanteq.assingment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssingmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAssingmentApplication.class, args);
+	}
+}
